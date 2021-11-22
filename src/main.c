0df39310e24b54fc132b7772f3c9a44b9e3da0dd
Jon Escombe <jone@dresco.co.uk>
2021-11-22 09:43:55 +0000

    Migrate from printk to logging subsystem

--- src/main.c
+++ src/main.c
@@ -2,18 +2,21 @@
 #include <adp5589.h>
 #include <quadrature.h>
 
+#include <logging/log.h>
+LOG_MODULE_REGISTER(panel, LOG_LEVEL_INF);
+
 void main(void)
 {
     const struct device *i2c_dev;
     static uint8_t key_data[16]  = {0};
     static uint32_t prev_encoder;
 
-    printk("Hello World...! %s\n", CONFIG_BOARD);
+    LOG_INF("Hello World...! %s", CONFIG_BOARD);
 
     if (adp5589_init(&i2c_dev) != 0) {
-        printk("Error initialising ADP5589 device.\n");
+        LOG_ERR("Error initialising ADP5589 device.");
     } else {
-        printk("ADP5589 initialised successfully.\n");
+        LOG_INF("ADP5589 initialised successfully.");
     }
 
     quadrature_init();
@@ -26,14 +29,14 @@ void main(void)
         uint8_t event_count =  adp5589_get_event_count(i2c_dev);
 
         if (event_count) {
-            //printk("Event count %u, reading data..\n", event_count);
+            //LOG_INF("Event count %u, reading data..", event_count);
 
             adp5589_get_register_values(i2c_dev, ADP5589_ADR_FIFO10, key_data, event_count);
             for (int i = 0; i < event_count; i++) {
                 if (key_data[i] & 0x80)
-                    printk("Key 0x%02X pressed..\n", (key_data[i] & 0x7F));
+                    LOG_INF("Key 0x%02X pressed..", (key_data[i] & 0x7F));
                 else
-                    printk("Key 0x%02X released..\n", (key_data[i] & 0x7F));
+                    LOG_INF("Key 0x%02X released..", (key_data[i] & 0x7F));
             }
         }
 
@@ -42,7 +45,7 @@ void main(void)
         //
         uint32_t encoder = quadrature_get_value();
         if (encoder != prev_encoder)
-            printk("Encoder value is: %d\r\n", encoder);
+            LOG_INF("Encoder value is: %d", encoder);
         prev_encoder = encoder;
 
         k_msleep(100);
