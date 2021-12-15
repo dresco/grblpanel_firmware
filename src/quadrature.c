3b0ab319f19f639b37f2900fcbcd2427d631babb
Jon Escombe <jone@dresco.co.uk>
2021-12-15 15:30:29 +0000

    Add support for second quadrature encoder

--- src/quadrature.c
+++ src/quadrature.c
@@ -8,6 +8,7 @@
 void quadrature_init(void)
 {
     quadrature_tim2_ll_init();
+    quadrature_tim3_ll_init();
 }
 
 void quadrature_tim2_ll_init(void)
@@ -50,6 +51,49 @@ void quadrature_tim2_ll_init(void)
     LL_TIM_EnableCounter(TIM2);
 }
 
+void quadrature_tim3_ll_init(void)
+{
+
+    LL_TIM_InitTypeDef TIM_InitStruct = {0};
+    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3);
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+
+    // TIM3 GPIO Configuration
+    // PC6   ------> TIM3_CH1
+    // PC7   ------> TIM3_CH2
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_6|LL_GPIO_PIN_7;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_2;
+    LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    LL_TIM_SetEncoderMode(TIM3, LL_TIM_ENCODERMODE_X4_TI12);
+    LL_TIM_IC_SetActiveInput(TIM3, LL_TIM_CHANNEL_CH1, LL_TIM_ACTIVEINPUT_DIRECTTI);
+    LL_TIM_IC_SetPrescaler(TIM3, LL_TIM_CHANNEL_CH1, LL_TIM_ICPSC_DIV1);
+    LL_TIM_IC_SetFilter(TIM3, LL_TIM_CHANNEL_CH1, LL_TIM_IC_FILTER_FDIV8_N6);
+    LL_TIM_IC_SetPolarity(TIM3, LL_TIM_CHANNEL_CH1, LL_TIM_IC_POLARITY_RISING);
+    LL_TIM_IC_SetActiveInput(TIM3, LL_TIM_CHANNEL_CH2, LL_TIM_ACTIVEINPUT_DIRECTTI);
+    LL_TIM_IC_SetPrescaler(TIM3, LL_TIM_CHANNEL_CH2, LL_TIM_ICPSC_DIV1);
+    LL_TIM_IC_SetFilter(TIM3, LL_TIM_CHANNEL_CH2, LL_TIM_IC_FILTER_FDIV8_N6);
+    LL_TIM_IC_SetPolarity(TIM3, LL_TIM_CHANNEL_CH2, LL_TIM_IC_POLARITY_RISING);
+    TIM_InitStruct.Prescaler = 0;
+    TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
+    TIM_InitStruct.Autoreload = 65535;
+    TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
+    LL_TIM_Init(TIM3, &TIM_InitStruct);
+    LL_TIM_DisableARRPreload(TIM3);
+    LL_TIM_SetTriggerOutput(TIM3, LL_TIM_TRGO_RESET);
+    LL_TIM_DisableMasterSlaveMode(TIM3);
+
+    LL_TIM_EnableCounter(TIM3);
+}
+
+
+
 uint16_t quadrature_get_value(uint8_t instance)
 {
     switch (instance) {
@@ -57,7 +101,11 @@ uint16_t quadrature_get_value(uint8_t instance)
             return LL_TIM_GetCounter(TIM2);
             break;
 
-        default:
+        case 2:
+            return LL_TIM_GetCounter(TIM3);
+            break;
+
+       default:
             return 0;
     }
 }
